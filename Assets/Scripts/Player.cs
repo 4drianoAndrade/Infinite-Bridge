312047dc1af6454ae819410f5408ceb2105fcd4e
Adriano Andrade <adrandrade1995@gmail.com>
2023-03-20 13:33:00 -0300

    Limiting the player's movement on the bridge in the X and Y axes, and shadowing the bridge as he runs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,11 @@ public class Player : MonoBehaviour
     [Header("Player Settings")]
     public float movementSpeedPlayer;
 
+    public float limitMovementMax_X;
+    public float limitMovementMin_X;
+    public float limitMovementMax_Y;
+    public float limitMovementMin_Y;
+
     private void Awake()
     {
         rb2DPlayerComponent = GetComponent<Rigidbody2D>();
@@ -21,9 +26,38 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        MovementPlayer();
+    }
+
+    private void MovementPlayer()
+    {
+        // Old Unity input system
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
 
+        float currentPositionPlayerX = transform.position.x;
+        float currentPositionPlayerY = transform.position.y;
+
         rb2DPlayerComponent.velocity = new Vector2(horizontalInput * movementSpeedPlayer, verticalInput * movementSpeedPlayer);
+
+        if (transform.position.x >= limitMovementMax_X)
+        {
+            currentPositionPlayerX = limitMovementMax_X;
+        }
+        else if (transform.position.x <= limitMovementMin_X)
+        {
+            currentPositionPlayerX = limitMovementMin_X;
+        }
+
+        if (transform.position.y >= limitMovementMax_Y)
+        {
+            currentPositionPlayerY = limitMovementMax_Y;
+        }
+        else if (transform.position.y <= limitMovementMin_Y)
+        {
+            currentPositionPlayerY = limitMovementMin_Y;
+        }
+
+        transform.position = new Vector2(currentPositionPlayerX, currentPositionPlayerY);
     }
 }
